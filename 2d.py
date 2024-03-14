40831908dfcd50ccfbe9978253e66c6ed412ec64
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-03-14 23:28:20 +0530

    Add files via upload

---
+++ 2d.py
@@ -0,0 +1,9 @@
+# Input two characters from the user
+char1 = input("Enter the first character: ")
+char2 = input("Enter the second character: ")
+
+# Check if the characters are equal
+if char1 == char2:
+    print("The characters are equal.")
+else:
+    print("The characters are not equal.")
