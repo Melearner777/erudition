f4f8b81f58c5be4bba75addcc5504e471a925eca
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-03-14 23:28:26 +0530

    Add files via upload

---
+++ alphabates.py
@@ -0,0 +1,10 @@
+
+char = input("Enter a character: ")
+
+if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u':
+    print(" this word is vowel.")
+else:
+    print("  this word is a consonant.")
+
+
+
