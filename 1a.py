bc8747604a4529dc1237b2dd03c6be54b96dbbf0
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-03-14 23:27:57 +0530

    Add files via upload

---
+++ 1a.py
@@ -0,0 +1,7 @@
+sum_of_natural_numbers=0
+n=int(input("Enter the number: "))
+for n in range(1,n+1):
+    sum_of_natural_numbers += n
+
+
+print("The sum of the natural numbers is:", sum_of_natural_numbers)
